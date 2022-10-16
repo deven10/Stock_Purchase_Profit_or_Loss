0385ae688d20199e2e84cb550af7797187e7323f
Deven Umrania <32352481+deven10@users.noreply.github.com>
2022-10-16 20:48:26 +0530

    Handled Negative Values

--- app.js
+++ app.js
@@ -16,33 +16,77 @@ submitButton.addEventListener("click", submitHandler);
 }*/
 
 function submitHandler(){
+    
     let ip = Number(initialPrice.value);
     let qty = Number(stocksQuantity.value);
     let curr = Number(currentPrice.value);
 
-    calculateProfitAndLoss(ip, qty, curr);
+    checkProfitOrLoss(ip, qty, curr);
 }
 
-function calculateProfitAndLoss(initial, quantity, current){
-    if(initial > current){
-        const loss = (initial - current) * quantity;
-        const fixedLoss = Math.round(loss);
-        const lossPercentage = (loss/initial) * 100;
-        const fixedLossPercentage = Math.round(lossPercentage);
-       showOutput(`" 😖 Your loss is ${fixedLoss} and loss percentage is ${fixedLossPercentage}% "`);
-    } else if(current > initial){
+function checkProfitOrLoss(initial, quantity, current){
+    let x = current - initial;
+    console.log("profit or loss is =>", x);
+    let y = x / initial;
+    let percentage = y * 100;
+    console.log(percentage, "<= percentage");
+
+    if(initial < 0 || current < 0 || quantity < 0){
+        showOutput(`" Please Enter Valid Inputs, Inputs cannot be Empty or Negative "`);
+    }
+
+    else if(x > 0){  // If you have made profit run this 
         const profit = (current - initial) * quantity;
         const fixedProfit = Math.round(profit);
-        const profitPercentage = (profit/initial) * 100;
-        const fixedProfitPercentage = Math.round(profitPercentage);
-       showOutput(`" Hey! 😍 Your Profit is ${fixedProfit} and profit percentage is ${fixedProfitPercentage}% "`);
-    } else{
-       showOutput(`" No pain no gain and no gain no pain "`);
+        const profitPercentage = Math.round(percentage);
+        console.log("yaya profit");
+       showOutput(`" Hey! 😍 Your Profit is ${fixedProfit} and profit percentage is ${profitPercentage}% "`);
+    } 
+
+    else if(x < 0) { // If you have made profit run this
+        const loss = (initial - current) * quantity;
+        const fixedLoss = Math.round(loss);
+        const fixedLossPercentage = Math.round(percentage);
+        console.log("loss sorry");
+       showOutput(`" 😖 Your loss is ${fixedLoss} and loss percentage is ${fixedLossPercentage}% "`);
     }
+
+    else{
+        showOutput(`" No pain no gain and no gain no pain "`);
+     }
+
 }
 
+// function calculateProfitAndLoss(initial, quantity, current){
+
+//     if(initial < 0 || current < 0 || quantity < 0){
+//         showOutput(`" Please Enter Valid Inputs, Inputs cannot be Empty or Negative "`);
+
+//     } else if(initial > current){
+
+//         const loss = (initial - current) * quantity;
+//         const fixedLoss = Math.round(loss);
+//         const lossPercentage = (loss/initial) * 100;
+//         const fixedLossPercentage = Math.round(lossPercentage);
+//        showOutput(`" 😖 Your loss is ${fixedLoss} and loss percentage is ${fixedLossPercentage}% "`);
+
+//     } else if(current > initial){
+
+//         const profit = (current - initial) * quantity;
+//         const fixedProfit = Math.round(profit);
+//         const profitPercentage = (profit/initial) * 100;
+//         const fixedProfitPercentage = Math.round(profitPercentage);
+//        showOutput(`" Hey! 😍 Your Profit is ${fixedProfit} and profit percentage is ${fixedProfitPercentage}% "`);
+
+//     } else{
+
+//        showOutput(`" No pain no gain and no gain no pain "`);
+
+//     }
+// }
+
 function showOutput(message){
-    outputBox.innerHTML = message;
+    outputBox.innerText = message;
 }
 
 
